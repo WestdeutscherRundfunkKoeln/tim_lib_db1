1c93650f87e29872c61447fce0fa6e3ded35847a
Bayram Guenes <bayram.guenes@ivz.de>
2024-08-27 17:35:02 +0200

    tim_enrich_rela

--- pkg/db_svc_repo/database_repo.go
+++ pkg/db_svc_repo/database_repo.go
@@ -708,7 +708,7 @@ func CreateDatabaseTables(iUseDriver string, iConnection, iDatabase string, iSet
 		"timecreate varchar(15)," +
 		"timeactive varchar(15)," +
 		"uname varchar(30)," +
-		"primary key (queabk, whenattrname, whenattrval, whenvalrela))"
+		"primary key (queabk,whenattrname,whenattrval,whenvalrela))"
 
 	dbsys.CreateTable(iUseDriver, db, oraDB, lvTable, lvFields)
 
